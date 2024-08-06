e3f5f3b04b41ea69333948e83f0b73a0c114a8c3
Richard Jean-Baptiste <Richinbk1@gmail.com>
2024-08-06 00:18:50 -0400

    added responsive styling for frontpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 //import React from 'react';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useTheme }  from '@mui/material/styles';
 import Box from '@mui/material/Box';
+import TextField  from "@mui/material/TextField";
 import Typography  from "@mui/material/Typography";
-
+import InputAdornment from '@mui/material/InputAdornment';
+import IconButton  from "@mui/material/IconButton";
+import SearchIcon from '@mui/icons-material/Search';
 
 
 const Header = () => {
@@ -14,12 +15,29 @@ const Header = () => {
 
     return(
         <Box sx={styles.header}>
-            <Box style={styles.headerItems}>
-                <Typography component="p" sx={{ marginLeft: '5%'}}>Home</Typography>
-                <Typography component="p" sx={{ marginLeft: '7%'}}>About</Typography>
-                <Box sx={{ marginTop: '1.5%', marginLeft: '10%'}}>
-                    <FontAwesomeIcon icon={faMagnifyingGlass} />
-                </Box> 
+            <Box sx={styles.headerItems}>
+                <Typography component="p" sx={styles.headerItem1}>Home</Typography>
+                <Typography component="p" sx={styles.headerItem2}>About</Typography>
+                <TextField id="filled-basic" label="Search" variant="filled" 
+                    sx={styles.headerForm}
+                    InputLabelProps={{
+                        sx: {
+                            top: ['-7px','-8px','-8px' ,'-4px','-3px'],
+                            fontSize: ['12px','12px','14px', '17px', '14px'],
+                        }
+                    }}
+                    InputProps={{
+                        endAdornment: 
+                            <InputAdornment position="end">
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                edge="end"
+                                >
+                                    <SearchIcon/>
+                                </IconButton>
+                        </InputAdornment>
+                    }}
+                />
             </Box>
         </Box>
     )
@@ -38,9 +56,25 @@ const useStyles = (theme) => ({
         display: 'flex',
         flexDirection: 'row',
         height: '100%',
-        width: '15%',
-        marginLeft: '85%',
+        width: '100%',
+        //marginLeft: ['63%','80%','85%','88%','90%'],
         marginTop: '.6%'
+    },
+    headerForm: {
+        borderRadius: '20px', 
+        backgroundColor: 'white',
+        width: ['35%', '35%', '18%','21%','20%'],
+        height: '77%',
+        marginTop: '-0.4%',
+        marginLeft: '2%'
+    },
+    headerItem1: {
+        marginLeft: ['37%','47%', '69%', '69%', '69%'], 
+        marginTop: '.5%'
+    },
+    headerItem2: {
+        marginLeft: ['3%','1%','1%','1%','1%'], 
+        marginTop: '.5%'
     }
 })
 
